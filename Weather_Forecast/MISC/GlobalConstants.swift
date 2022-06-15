196e39ec88a42a9b7994bc16668d3ce020a62976
신의연 <dusdl14@gmail.com>
2022-06-15 20:00:34 +0900

    Create GlobalConstraints for Common Constraints

--- Weather_Forecast/MISC/GlobalConstants.swift
+++ Weather_Forecast/MISC/GlobalConstants.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+import UIKit
+
+enum GlobalConstants {
+    
+        enum Color {
+            enum Background {
+                static var day: UIColor = UIColor(red: 44/255, green: 186/255, blue: 246/255, alpha: 1)
+                static var night: UIColor = UIColor(red: 24/255, green: 28/255, blue: 115/255, alpha: 1)
+                static var gray: UIColor = UIColor(red: 149/255, green: 150/255, blue: 158/255, alpha: 1)
+            }
+    }
+}
+
