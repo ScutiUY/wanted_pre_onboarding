3cbb193ca9297d98daed90f3cc8eadd43e1b1ba0
신의연 <dusdl14@gmail.com>
2022-06-16 20:44:27 +0900

    fix decoding codingkeys for exception

--- Weather_Forecast/Model/Main.swift
+++ Weather_Forecast/Model/Main.swift
@@ -19,4 +19,35 @@ struct Main: Codable {
     let temp_max: Float
     let pressure: Float
     let humidity: Int
+    
+    init(temp: Float, feels_like: Float, temp_min: Float, temp_max: Float, pressure: Float, humidity: Int) {
+        self.temp = temp
+        self.feels_like = feels_like
+        self.temp_min = temp_min
+        self.temp_max = temp_max
+        self.pressure = pressure
+        self.humidity = humidity
+    }
+    
+    enum Codingkeys: CodingKey {
+        case temp
+        case feels_like
+        case temp_min
+        case temp_max
+        case pressure
+        case humidity
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: Codingkeys.self)
+        temp = (try? container.decode(Float.self, forKey: .temp)) ?? 0.0
+        feels_like = (try? container.decode(Float.self, forKey: .feels_like)) ?? 0.0
+        temp_min = (try? container.decode(Float.self, forKey: .temp_min)) ?? 0.0
+        temp_max = (try? container.decode(Float.self, forKey: .temp_max)) ?? 0.0
+        pressure = (try? container.decode(Float.self, forKey: .temp)) ?? 0.0
+        humidity = (try? container.decode(Int.self, forKey: .humidity)) ?? 0
+        
+    }
+    
 }
