465b805d74cb5684ea794f70defb1d2d305d6083
신의연 <dusdl14@gmail.com>
2022-06-16 20:44:44 +0900

    fix decoding codingkeys for exception

--- Weather_Forecast/Model/Sys.swift
+++ Weather_Forecast/Model/Sys.swift
@@ -8,9 +8,38 @@
 import Foundation
 
 struct Sys: Codable {
+    
     var type: Int
     var id: Int
     var country: String
     var sunrise: Int
     var sunset: Int
+    
+    enum Codingkeys: CodingKey {
+        case type
+        case id
+        case country
+        case sunrise
+        case sunset
+    }
+    
+    init(type: Int, id: Int, country: String, sunrise: Int, sunset: Int) {
+        self.type = type
+        self.id = id
+        self.country = country
+        self.sunrise = sunrise
+        self.sunset = sunset
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: Codingkeys.self)
+        type = (try? container.decode(Int.self, forKey: .type)) ?? 0
+        id = (try? container.decode(Int.self, forKey: .type)) ?? 0
+        country = (try? container.decode(String.self, forKey: .type)) ?? ""
+        sunrise = (try? container.decode(Int.self, forKey: .type)) ?? 0
+        sunset = (try? container.decode(Int.self, forKey: .type)) ?? 0
+        
+    }
+    
 }
